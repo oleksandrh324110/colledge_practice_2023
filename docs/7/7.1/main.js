e4fdc229248679c96fa44e25ba333ff75a3a778b
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2023-03-07 22:14:15 +0200

    hwat

--- docs/7/7.1/main.js
+++ docs/7/7.1/main.js
@@ -7,7 +7,7 @@ const matrix = math.matrix([
   [4, 4, 1, -5]
 ])
 
-const extraMatrix = math.matrix([3, 0, 6, 11])
+const extraMatrix = math.matrix([[3], [0], [6], [11]])
 
 result.innerHTML += '\tВипишемо всі коефіцієнти в матрицю: `[[1, 1, -3, 1, |, 3], [2, 5, -10, -3, |, 0], [1, -2, 3, 4, |, 6], [4, 4, 1, -5, |, 11]]`'
 result.innerHTML += '\n\n\tЗнайдемо обернену матрицую для матриці \`[[1, 1, -3, 1], [2, 5, -10, -3], [1, -2, 3, 4], [4, 4, 1, -5]]\`:'
@@ -15,4 +15,4 @@ result.innerHTML += '\n\n\tЗнайдемо обернену матрицую д
 const inv = math.inv(matrix).map(v => +v.toFixed(2))
 result.innerHTML += `\n\n\t\t\`[[1, 1, -3, 1], [2, 5, -10, -3], [1, -2, 3, 4], [4, 4, 1, -5]]^-1 = ${inv}\``
 
-result.innerHTML += `\n\n\t\t\`X = ${inv} * [[3], [0], [6], [11]] = ${math.multiply(inv, [[3], [0], [6], [11]]).map(v => +v.toFixed(2))}\``
+result.innerHTML += `\n\n\t\t\`X = ${inv} * [[3], [0], [6], [11]] = ${math.multiply(inv, extraMatrix).map(v => +v.toFixed(2))}\``
---
+++ docs/7/7.4/main.js
@@ -0,0 +1,20 @@
+const result = document.getElementById('result')
+
+let leftMatrix = math.matrix([
+  [1, 2, 0],
+  [2, 5, -2],
+  [0, -2, 5]
+])
+
+const rightMatrix = math.matrix([
+  [1, 0, 0],
+  [0, 1, 1],
+  [0, 1, 0]
+])
+
+leftMatrix = math.inv(leftMatrix)
+
+result.innerHTML +=
+  `\tЗараз ми маємо рівняння виду \`A * X = C\`, де \`X = A^-1*C\`
+   \n\t\t\`[[1, 2, 0],[2, 5, -2], [0, -2, 5]]^-1 = ${leftMatrix}\`
+   \n\t\t\`X = ${leftMatrix} * ${rightMatrix} = ${math.multiply(leftMatrix, rightMatrix)}\``
